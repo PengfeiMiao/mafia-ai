f2b4249a936682e0428568eea074d9ad91e79b53
PengfeiMiao <2018224980@qq.com>
2025-03-09 23:57:27 +0800

    add BackgroundMask

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Button, Flex, Input, Text} from '@chakra-ui/react';
 import {setCookie} from "@/store/CacheStore";
 import {loginApi} from "@/api/api";
+import BackgroundMask from "@/components/BackgroundMask";
 
 const LoginPage = () => {
   const [password, setPassword] = useState('');
@@ -17,10 +18,20 @@ const LoginPage = () => {
     }
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      handleLogin().then();
+    }
+  };
+
   return (
     <Flex h="100vh" w="100vw" pt={16} justify="flex-start" align="center" direction="column">
-      <Text fontSize="xl" fontWeight="bold" mb={28}>Mafia AI</Text>
-      <Flex p={8} bgColor={'purple.subtle'} borderRadius="xl" direction="column">
+      <Flex zIndex={0}>
+        <BackgroundMask/>
+      </Flex>
+      <Text fontSize="xl" fontWeight="bold" mb={28} zIndex={1}>Mafia AI</Text>
+      <Flex p={8} bgColor={'purple.subtle'} borderRadius="xl" direction="column" zIndex={1} opacity={0.9}>
         <Flex mb={8} justify="center" align="center" direction="row">
           <Text fontSize="md" fontWeight="500" mr={4}>Username</Text>
           <Input w={'20vw'} mr={2} value={'mafiadev'} disabled>
@@ -28,11 +39,19 @@ const LoginPage = () => {
         </Flex>
         <Flex justify="center" align="center" direction="row">
           <Text fontSize="md" fontWeight="500" mr={4}>Password</Text>
-          <Input w={'20vw'} mr={2} value={password} onChange={(e) => setPassword(e.target.value)}>
+          <Input w={'20vw'} mr={2} value={password}
+                 onChange={(e) => {
+                   e.preventDefault();
+                   setPassword(e.target.value)
+                 }}
+                 onKeyDown={handleKeyDown}>
           </Input>
         </Flex>
       </Flex>
-      <Button w={'12vw'} mt={4} onClick={handleLogin}>Login</Button>
+      <Flex>
+        <Button w={'12vw'} mt={4} mr={4} onClick={handleLogin}>Login</Button>
+        <Button w={'12vw'} mt={4} ml={4} onClick={handleLogin}>Sign up with email</Button>
+      </Flex>
     </Flex>
   );
 }
